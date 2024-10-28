2bb9d80b42d91305f1ed5a33b84ad9af678a838e
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2024-10-28 16:41:39 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        if(a != b && b != c && c != a)
+            return a + b + c;
+        else {
+            if(a == b && a == c) 
+                return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+            else {
+                if(a == b || a == c || b == c) {
+                    return (a + b + c) * (a*a + b*b + c*c);
+                }
+            }
+        }
+        
+        return 0; 
+    }
+}
