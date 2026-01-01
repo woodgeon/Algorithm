7920cdb3b79f257494ee8893ef2913e2af9fd183
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-01-01 16:11:43 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.13 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < n; i++) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
