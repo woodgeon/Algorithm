5536fd18988ec1a50d97799fc7ea6a6dce70bfdb
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-01-15 14:16:33 +0900

    [Silver IV] Title: solved.ac, Time: 320 ms, Memory: 37668 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -13,30 +13,23 @@ class Main {
         }
 
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
+
         int k = (int) Math.round(n * 0.15);
+        int start = k;
+        int end = n - k;
+        int cnt = end - start;
 
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < arr.length; i++) {
-            deque.offerLast(arr[i]);
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += arr[i];
         }
+        long avg = Math.round((double) sum / cnt);
+        System.out.println(avg);
 
-        for (int i = 0; i < k; i++) {
-            deque.pollFirst();
-            deque.pollLast();
-        }
-
-        double sum = 0;
-        int i = 0;
-        while (!deque.isEmpty()) {
-            sum += deque.pollFirst();
-            i++;
-        }
-        System.out.println(Math.round(sum / i));
 
     }
 }
