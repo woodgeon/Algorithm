4ef5f191dfb4116efa63a3bca4fa24fd6eb301af
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-05-16 16:31:52 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (140ms, 10MB)

---
+++ CodeTree/trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        printSqure(n, m);
+    }
+    static void printSqure(int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
