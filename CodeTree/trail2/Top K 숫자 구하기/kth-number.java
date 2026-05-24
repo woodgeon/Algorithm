3c44688179e7f7750ba236f8aeed98a096753ca0
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-05-24 23:21:17 +0900

    [Passed] Top K 숫자 구하기 (179ms, 12MB)

---
+++ CodeTree/trail2/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[m - 1]);
+    }
+}
