d75b49897b5a823f292f3bcf53b879dff42e9e0e
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-07-12 22:55:54 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,13 @@
+select to_char(sales_date, 'YYYY-MM-DD') as sales_date, product_id, user_id, sales_amount
+from online_sale 
+where sales_date >= to_date('2022-03-01', 'YYYY-MM-DD')
+and sales_date < to_date('2022-04-01', 'YYYY-MM-DD')
+
+union all
+
+select to_char(sales_date, 'YYYY-MM-DD') as sales_date, product_id, NULL as user_id, sales_amount
+from offline_sale 
+where sales_date >= to_date('2022-03-01', 'YYYY-MM-DD')
+and sales_date < to_date('2022-04-01', 'YYYY-MM-DD')
+
+order by sales_date asc, product_id asc, user_id asc;
