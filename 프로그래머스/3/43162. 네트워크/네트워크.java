9f6ad3898a0512f2f6ecf30544ccd19a7bf9ce6a
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-07-14 14:42:03 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,32 @@
 class Solution {
-    private static boolean[] visited;
-    private static int[][] computer;
     
-    private static void dfs(int now) {
-        visited[now] = true;
-        for (int i = 0; i < computer[now].length; i++) {
-            // 연결되어 있으며 방문하지 않은 노드라면
-            if (!visited[i] && computer[now][i] == 1) {
-                dfs(i);
-            }
-        }
-    }
+    static int[][] graph;
+    static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        computer = computers;
+        
+        graph = computers;
         visited = new boolean[n];
+        
+        for (int current = 0; current < n; current++) {
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i);
+            if (!visited[current]) {
+                dfs(current, n);
                 answer++;
             }
         }
         
         return answer;
+    }
+    
+    static void dfs(int current, int n) {
+        visited[current] = true;
+        
+        for (int i = 0; i < n; i++) {
+            if (graph[current][i] != 0 && !visited[i]) {
+                dfs(i, n);
+            }
+        }
     }
 }
