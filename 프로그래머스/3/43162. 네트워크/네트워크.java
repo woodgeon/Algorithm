f939942e480559bc40866149ba3a421da66c5a03
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-07-22 10:14:14 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,16 +1,22 @@
+import java.util.*;
+
 class Solution {
     
-    static int[][] graph;
+    static int[][] grid;
     static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        graph = computers;
+        grid = new int[n][n];
         visited = new boolean[n];
         
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[0].length; j++) {
+                grid[i][j] = computers[i][j];
+            }
+        }
+        
         for (int current = 0; current < n; current++) {
-
             if (!visited[current]) {
                 dfs(current, n);
                 answer++;
@@ -24,7 +30,7 @@ class Solution {
         visited[current] = true;
         
         for (int i = 0; i < n; i++) {
-            if (graph[current][i] != 0 && !visited[i]) {
+            if (grid[current][i] != 0 && !visited[i]) {
                 dfs(i, n);
             }
         }
