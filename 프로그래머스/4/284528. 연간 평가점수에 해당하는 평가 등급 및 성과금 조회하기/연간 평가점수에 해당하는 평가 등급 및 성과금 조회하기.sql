c37fa034f8db26f1cc333bedcfd1270e603597ff
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-08-07 11:56:11 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,18 @@
+select e.emp_no, e.emp_name, 
+    case
+        when (avg(g.score) >= 96) then 'S'
+        when (avg(g.score) >= 90) then 'A'
+        when (avg(g.score) >= 80) then 'B'
+        else 'C'
+    end as "GRADE",
+    case
+         when (avg(g.score) >= 96) then e.sal * 0.2
+        when (avg(g.score) >= 90) then e.sal * 0.15
+        when (avg(g.score) >= 80) then e.sal * 0.1
+        else e.sal * 0
+    end as 'BONUS'
+from hr_employees e join hr_grade g
+on e.emp_no = g.emp_no
+group by e.emp_no, e.emp_name
+having grade is not null
+order by e.emp_no asc;
