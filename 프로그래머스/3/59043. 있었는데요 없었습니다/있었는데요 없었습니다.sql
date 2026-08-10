4a64b43276ee9f3329b0d1529efc3a1acabbf1ca
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-08-10 18:32:18 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,5 @@
-select o.animal_id as "ANIMAL_ID", o.name as "NAME"
-from ANIMAL_INS i join ANIMAL_OUTS o
+select i.animal_id, i.name
+from animal_ins i join animal_outs o
 on i.animal_id = o.animal_id
-where i.datetime > o.datetime
+where i.datetime > o.datetime -- 날짜가 크다 = 더 오래 지났다
 order by i.datetime asc;
