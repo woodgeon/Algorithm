736ae9e0ec8f87d8a1c92be11d4f90ba25a54c0d
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-11 17:31:18 +0900

    [Algo Room] 취소되지 않은 진료 예약 조회하기 (MySQL)

---
+++ algoroom/sql/pg-132204/mysql/solution.sql
@@ -0,0 +1,9 @@
+select a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
+from APPOINTMENT a join DOCTOR d
+on a.MDDR_ID = d.DR_ID
+join PATIENT p
+on a.PT_NO = p.PT_NO
+where a.MCDP_CD = 'CS' 
+    and APNT_CNCL_YN = 'N'
+    and date_format(APNT_YMD, '%Y-%m-%d') = '2022-04-13'
+order by a.APNT_YMD asc;
