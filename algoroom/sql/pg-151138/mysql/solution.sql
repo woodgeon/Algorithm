8d1839f135b97ae1a847accb8e12173704ea6665
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-11 11:46:59 +0900

    [Algo Room] 자동차 대여 기록에서 장기/단기 대여 구분하기 (MySQL)

---
+++ algoroom/sql/pg-151138/mysql/solution.sql
@@ -0,0 +1,8 @@
+select HISTORY_ID, CAR_ID, START_DATE, END_DATE,
+    case
+        when END_DATE - START_DATE + 1 >= 30 then '장기 대여'
+        else '단기 대여'
+    end as "RENT_TYPE"
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where START_DATE between '2022-09-01' and '2022-09-30'
+order by history_id desc;
