4d188dff70bf5a3b015f61ce6efee1a179b1c413
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-11 15:37:30 +0900

    [Algo Room] 자동차 평균 대여 기간 구하기 (MySQL)

---
+++ algoroom/sql/pg-157342/mysql/solution.sql
@@ -0,0 +1,5 @@
+select car_id, round(avg(datediff(END_DATE, START_DATE) + 1), 1) as "AVERAGE_DURATION"
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by car_id
+having AVERAGE_DURATION >= 7
+order by AVERAGE_DURATION desc, car_id desc;
