974be362517ff8fae8f737a44222ae8e2a740374
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 07:47:13 +0900

    [Algo Room] 부모의 형질을 모두 가지는 대장균 찾기 (MySQL)

--- algoroom/sql/pg-301647/mysql/solution.sql
+++ algoroom/sql/pg-301647/mysql/solution.sql
@@ -1,5 +1,6 @@
-select a.id, a.genotype, b.genotype as "PARENT_GENOTYPE"
-from ecoli_data a join ecoli_data b
-on a.parent_id = b.id
-where a.genotype & b.genotype = b.genotype
-order by id asc; 
+select child.id as ID, child.genotype as GENOTYPE, parent.genotype as PARENT_GENOTYPE
+from ecoli_data parent join ecoli_data child
+on parent.id = child.parent_id
+-- 부모의 형질을 모두 가지려면, 부모와 and 연산했을 때, 부모 비트만큼 나오면 다 가지고있는거
+where child.genotype & parent.genotype = parent.genotype
+order by child.id asc;
