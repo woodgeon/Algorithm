55097b2b1ba366eb0449a4fcd58390f75d2a3825
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-13 23:27:06 +0900

    [Algo Room] 서울에 위치한 식당 목록 출력하기 (MySQL)

---
+++ algoroom/sql/pg-131118/mysql/solution.sql
@@ -0,0 +1,9 @@
+select i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, r.SCORE
+from REST_INFO i join (
+    select REST_ID, round(avg(REVIEW_SCORE), 2) as "SCORE"
+    from REST_REVIEW
+    group by REST_ID
+) r
+on i.REST_ID = r.REST_ID
+where i.address like '%서울%'
+order by i.FAVORITES desc;
