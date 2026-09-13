5d43e1149e4a58de7c3bd130a0c34a73e606314f
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 04:38:27 +0900

    [Algo Room] 오프라인/온라인 판매 데이터 통합하기 (MySQL)

--- algoroom/sql/pg-131537/mysql/solution.sql
+++ algoroom/sql/pg-131537/mysql/solution.sql
@@ -1,10 +1,10 @@
 select SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
 from ONLINE_SALE
-where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3
+where SALES_DATE like '2022-03%'
 
-union all 
+union all
 
 select SALES_DATE, PRODUCT_ID, null as USER_ID, SALES_AMOUNT
 from OFFLINE_SALE
-where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3
-order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc;
+where SALES_DATE like '2022-03%'
+order by SALES_DATE, PRODUCT_ID, USER_ID;
