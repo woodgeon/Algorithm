309060284eb7f73c9edb23431061a6ba57fcabfc
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 07:34:03 +0900

    [Algo Room] 업그레이드 된 아이템 구하기 (MySQL)

--- algoroom/sql/pg-273711/mysql/solution.sql
+++ algoroom/sql/pg-273711/mysql/solution.sql
@@ -1,9 +1,14 @@
 select item_id, item_name, rarity
 from item_info
+-- 해당 아이템 트리의 아이디를
 where item_id in (
-    select t.item_id
-    from item_info i join item_tree t
-    on i.item_id = t.parent_item_id
-    where i.rarity = 'RARE'
+    select item_id
+    from item_tree
+    -- 부모가 레어인 아이템을 찾아서
+    where parent_item_id in (
+        select item_id
+        from item_info
+        where rarity = 'RARE'
+    )
 )
-order by item_id desc, item_name desc, rarity desc;
+order by item_id desc;
