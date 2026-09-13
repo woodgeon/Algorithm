abc1bfbd1dc7207852bd5d8167faee1950c68395
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 08:44:19 +0900

    [Algo Room] 진료과별 총 예약 횟수 출력하기 (MySQL)

--- algoroom/sql/pg-132202/mysql/solution.sql
+++ algoroom/sql/pg-132202/mysql/solution.sql
@@ -1,6 +1,5 @@
-select mcdp_cd as "진료과코드", count(*) as "5월예약건수"
-from appointment
-where apnt_ymd between date '2022-05-01' and date '2022-05-30'
-    and apnt_cncl_yn = 'N'
-group by mcdp_cd
-order by 5월예약건수 asc, mcdp_cd asc;
+select MCDP_CD as "진료과코드", count(*) as "5월예약건수"
+from APPOINTMENT
+where APNT_YMD like '2022-05%'
+group by MCDP_CD
+order by 5월예약건수 asc, MCDP_CD asc;
