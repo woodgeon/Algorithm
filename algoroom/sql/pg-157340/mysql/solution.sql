9ff7366d49130e3ba6154fb3be657da2c5f5eb96
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 08:32:15 +0900

    [Algo Room] 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기 (MySQL)

--- algoroom/sql/pg-157340/mysql/solution.sql
+++ algoroom/sql/pg-157340/mysql/solution.sql
@@ -1,12 +1,8 @@
-select car_id as "CAR_ID",
+select car_id, 
     case
-        when car_id in (
-            select car_id
-            from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-            where '2022-10-16' between start_date and end_date
-        ) then '대여중'
+        when sum('2022-10-16' between start_date and end_date) > 0 then '대여중'
         else '대여 가능'
-    end as "AVAILABILITY" 
+    end as "AVAILABILITY"
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
 group by car_id
 order by car_id desc;
