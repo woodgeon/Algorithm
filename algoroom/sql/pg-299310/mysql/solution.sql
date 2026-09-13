d407a54b1b729ec218a86d72e0e37168c7ff2450
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 08:26:05 +0900

    [Algo Room] 연도별 대장균 크기의 편차 구하기 (MySQL)

--- algoroom/sql/pg-299310/mysql/solution.sql
+++ algoroom/sql/pg-299310/mysql/solution.sql
@@ -1,11 +1,8 @@
-with yearly_max as (
-    select year(differentiation_date) as "YEAR", max(SIZE_OF_COLONY) as "MAX_SIZE"
+select year(e.DIFFERENTIATION_DATE) as YEAR, m.MAX - e.size_of_colony as "YEAR_DEV", e.id
+from ecoli_data e join (
+    select year(DIFFERENTIATION_DATE) as YEAR, max(size_of_colony) as MAX
     from ecoli_data
-    group by year(differentiation_date)
-)
-
-select year(e.DIFFERENTIATION_DATE) as "YEAR", (MAX_SIZE - e.SIZE_OF_COLONY) as "YEAR_DEV", e.id
-from ecoli_data e join yearly_max y
-on year(e.DIFFERENTIATION_DATE) = YEAR
+    group by year(DIFFERENTIATION_DATE)
+) m
+on year(e.DIFFERENTIATION_DATE) = m.YEAR
 order by YEAR asc, YEAR_DEV asc;
-
