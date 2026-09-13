e4204e925b5a059149683aa3793591b33c2a1794
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 08:58:04 +0900

    [Algo Room] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 (MySQL)

--- algoroom/sql/pg-151139/mysql/solution.sql
+++ algoroom/sql/pg-151139/mysql/solution.sql
@@ -1,12 +1,12 @@
-select month(START_DATE) as "MONTH", car_id as "CAR_ID", count(*) as "RECORDS"
+select month(START_DATE) as MONTH, car_id, count(*) as RECORDS
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where START_DATE between date '2022-08-01' and date '2022-10-31'
-group by month(START_DATE), car_id
-having car_id in (
+where car_id in (
     select car_id
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    where START_DATE between date '2022-08-01' and date '2022-10-31'
+    where START_DATE between '2022-08-01' and '2022-10-31'
     group by car_id
     having count(*) >= 5
 )
+and START_DATE between '2022-08-01' and '2022-10-31'
+group by month(START_DATE), car_id
 order by month(START_DATE) asc, car_id desc;
