13031bda6df51c2e695e7f69121828ddb6b3c48d
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 07:44:08 +0900

    [Algo Room] 특정 형질을 가지는 대장균 찾기 (MySQL)

--- algoroom/sql/pg-301646/mysql/solution.sql
+++ algoroom/sql/pg-301646/mysql/solution.sql
@@ -1,5 +1,6 @@
 select count(*) as "COUNT"
 from ecoli_data
-where genotype & 2 = 0
-    and (genotype & 1 != 0
-    or genotype & 4 != 0);
+where GENOTYPE & 2 = 0
+    and (GENOTYPE & 1 > 0 or GENOTYPE & 4 > 0);
+-- 2번 형질을 보유하지 않는다. &(and 연산으로 0이 나온다는 건, 일치하는 비트 없음.)
+-- 1번이나 3번 형질을 보유한다. &(and 연산으로 0보다 큰 건, 일치하는 비트 있음.)
