2f97453338e32610e8f7d6eee5a83b839344ec1a
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 08:42:33 +0900

    [Algo Room] 식품분류별 가장 비싼 식품의 정보 조회하기 (MySQL)

--- algoroom/sql/pg-131116/mysql/solution.sql
+++ algoroom/sql/pg-131116/mysql/solution.sql
@@ -1,8 +1,8 @@
-select f.category as "CATEGORY", f.price as "MAX_PRICE", f.product_name as "PRODUCT_NAME"
-from food_product f
-where f.price = (
-    select max(sub.price)
-    from food_product sub
-    where sub.category = f.category
-) and f.category in ('과자', '국', '김치', '식용유')
+select f.CATEGORY, f.price as MAX_PRICE, f.PRODUCT_NAME
+from FOOD_PRODUCT f
+where PRICE = (
+    select max(PRICE)
+    from FOOD_PRODUCT sub
+    where sub.CATEGORY = f.CATEGORY
+) and f.CATEGORY in ('과자', '국', '김치', '식용유')
 order by MAX_PRICE desc;
