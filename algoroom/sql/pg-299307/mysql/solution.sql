20300b27ef2426ad5e1a099507b8fa54f33f3fb6
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 07:40:38 +0900

    [Algo Room] 대장균의 크기에 따라 분류하기 1 (MySQL)

--- algoroom/sql/pg-299307/mysql/solution.sql
+++ algoroom/sql/pg-299307/mysql/solution.sql
@@ -1,7 +1,7 @@
 select id, 
     case
-        when ifnull(size_of_colony, 0) <= 100 then 'LOW'
-        when ifnull(size_of_colony, 0) <= 1000 then 'MEDIUM'
+        when SIZE_OF_COLONY <= 100 then 'LOW'
+        when SIZE_OF_COLONY <= 1000 then 'MEDIUM'
         else 'HIGH'
     end as "SIZE"
 from ecoli_data
