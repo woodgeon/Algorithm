7700dfdd62aaa60c79d73e473eec06d778e35c98
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 09:05:26 +0900

    [Algo Room] 업그레이드 할 수 없는 아이템 구하기 (MySQL)

--- algoroom/sql/pg-273712/mysql/solution.sql
+++ algoroom/sql/pg-273712/mysql/solution.sql
@@ -1,8 +1,8 @@
-select ITEM_ID, ITEM_NAME, RARITY
-from item_info
+select i.item_id, i.item_name, i.rarity 
+from item_info i
 where item_id not in (
-    select parent_item_id
+    select PARENT_ITEM_ID
     from item_tree
     where parent_item_id is not null
 )
-order by item_id desc;
+order by i.item_id desc;
