f8af13fb1c95d5bb510e5bf1ee8f92d689ffae36
dogeonwoo <147317355+woodgeon@users.noreply.github.com>
2026-09-14 09:01:59 +0900

    [Algo Room] 입양 시각 구하기(2) (MySQL)

--- algoroom/sql/pg-59413/mysql/solution.sql
+++ algoroom/sql/pg-59413/mysql/solution.sql
@@ -1,11 +1,13 @@
 with recursive cte as (
-    select 0 as "HOUR"
+    select 0 as HOUR
     union all
-    select hour + 1 from cte where hour < 23
+    select HOUR + 1
+    from cte
+    where hour < 23
 )
 
-select c.hour as "HOUR", ifnull(count(ANIMAL_ID), 0) as "COUNT"
-from cte c left join animal_outs a
-on c.hour = hour(a.datetime)
-group by hour
-order by hour asc;
+select c.HOUR, count(o.animal_id) as COUNT
+from cte c left join animal_outs o
+on c.HOUR = hour(o.DATETIME)
+group by c.HOUR
+order by c.HOUR asc;
